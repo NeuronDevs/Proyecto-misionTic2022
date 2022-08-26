7efa5a9f0059116b15e8145117dbf4910054fab6
Luis Olivares <luissolivaresp@gmail.com>
2022-08-25 20:37:19 -0500

    rever commit

--- Classes/Transaction.java
+++ Classes/Transaction.java
@@ -1,20 +1,21 @@
-package Classes;
+import java.util.Date;
 
 public class Transaction {
 
-        private  long id;
-        private  String concept;
-        private float amount;
-        private User user;
-        private Enterprise  enterprise;
-        private date createAt;
-        private date updateAt;
+    private long id;
+    private String concept;
+    private float amount;
+    private Employee user;
+    private Enterprise enterprise;
+    private Date createAt;
+    private Date updateAt;
 
-    public Transaction( ) {
+    public Transaction() {
 
     }
 
-    public Transaction(long id, String concept, float amount, User user, Enterprise enterprise, date createAt, date updateAt) {
+    public Transaction(long id, String concept, float amount, Employee user, Enterprise enterprise, Date createAt,
+            Date updateAt) {
         this.id = id;
         this.concept = concept;
         this.amount = amount;
@@ -25,59 +26,59 @@ public class Transaction {
     }
 
     public long getId() {
-            return id;
-        }
-
-        public void setId(long id) {
-            this.id = id;
-        }
-
-        public String getConcept() {
-            return concept;
-        }
-
-        public void setConcept(String concept) {
-            this.concept = concept;
-        }
-
-        public float getAmount() {
-            return amount;
-        }
-
-        public void setAmount(float amount) {
-            this.amount = amount;
-        }
-
-        public User getUser() {
-            return user;
-        }
-
-        public void setUser(User user) {
-            this.user = user;
-        }
-
-        public Enterprise getEnterprise() {
-            return enterprise;
-        }
-
-        public void setEnterprise(Enterprise enterprise) {
-            this.enterprise = enterprise;
-        }
-
-        public date getCreateAt() {
-            return createAt;
-        }
-
-        public void setCreateAt(date createAt) {
-            this.createAt = createAt;
-        }
-
-        public date getUpdateAt() {
-            return updateAt;
-        }
-
-        public void setUpdateAt(date updateAt) {
-            this.updateAt = updateAt;
-        }
-
+        return id;
     }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Employee getUser() {
+        return user;
+    }
+
+    public void setUser(Employee user) {
+        this.user = user;
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+}
