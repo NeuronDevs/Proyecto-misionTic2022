1edfda5e6f90f85e0c1ece499a6cde34d9db900d
Paucort <111184873+Paucort@users.noreply.github.com>
2022-09-01 21:52:49 -0500

    transaction controller created

---
+++ src/main/java/com/NeuronDevs/GestionFinanciera/Controllers/TransactionController.java
@@ -0,0 +1,33 @@
+
+package com.NeuronDevs.GestionFinanciera.Controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @GetMapping("")
+    public String getTransactions(){
+        return "you want to get all transactions";
+    }
+    @PostMapping("")
+    public String postTransaction(){
+        return "you want to post a transaction";
+    }
+
+    @GetMapping("/{id}")
+    public String getTransaction(@PathVariable Long id){
+        return "you want to get a transaction";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateTransaction(@PathVariable Long id){
+        return "you want to update a transaction";
+    }
+    @DeleteMapping("/{id}")
+    public String deleteTransaction(@PathVariable Long id){
+        return "you want to delete a transaction";
+    }
+}
+
