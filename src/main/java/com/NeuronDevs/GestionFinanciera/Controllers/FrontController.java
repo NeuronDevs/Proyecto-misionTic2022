c66261a90050175f6fd6d422c0a15c022b983411
sergioqp82 <sergioalejandroquintero82@gmail.com>
2022-09-14 21:54:14 -0500

    Creaciòn de interfaz para visualizar transacciones

--- src/main/java/com/NeuronDevs/GestionFinanciera/Controllers/FrontController.java
+++ src/main/java/com/NeuronDevs/GestionFinanciera/Controllers/FrontController.java
@@ -1,11 +1,16 @@
 package com.NeuronDevs.GestionFinanciera.Controllers;
 
+import com.NeuronDevs.GestionFinanciera.Entities.Enterprise;
+import com.NeuronDevs.GestionFinanciera.Entities.Transaction;
 import com.NeuronDevs.GestionFinanciera.Entities.User;
+import com.NeuronDevs.GestionFinanciera.Services.EnterpriseService;
+import com.NeuronDevs.GestionFinanciera.Services.TransactionService;
 import com.NeuronDevs.GestionFinanciera.Services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -14,6 +19,8 @@ import java.util.List;
 public class FrontController {
 
     UserService userService;
+    EnterpriseService enterpriseService;
+    TransactionService transactionService;
 
     @GetMapping("/")
     public String index(){
@@ -36,5 +43,17 @@ public class FrontController {
         return "nuevo_usuario";
     }
 
+    @GetMapping("/gestionar/transacciones/empresas")
+    public String listEnterprises(Model model){
+        List<Enterprise> enterprises =  this.enterpriseService.consultarEnterprise();
+        model.addAttribute("enterprises", enterprises);
+        return "enterprises";
+    }
+    @GetMapping("/gestionar/transacciones/empresa/{id}")
+    public String listTransactionsByEnterprise(Model model, @PathVariable Long id){
+        List<Transaction> transactions =  this.transactionService.getTransactions(id);
+        model.addAttribute("transactions", transactions);
+        return "transactions";
+    }
 
 }
